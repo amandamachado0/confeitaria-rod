fbe38c75250360a4f1bca23eb446367c6f9baaac
Amanda Machado <amandabuenomachado46@gmail.com>
2025-11-26 20:14:38 -0300

    criando div com js

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,32 @@
-    alert('oi foi carregado')
+
+
+    function criarItemCardapio(titulo, descricao, foto) {
+    const divItemCardapio = document.createElement('div')
+    divItemCardapio.className = 'item-cardapio'
+
+    const h3Titulo = document.createElement('h3')
+    h3Titulo.textContent = titulo
+
+    const pDescricao = document.createElement('p')
+    pDescricao.textContent = descricao
+    pDescricao.className = 'descricao'
+
+    const img = document.createElement('img')
+    img.src = foto 
+    img.className = 'img-item'
+
+    //adicionando os elementos na div mae 
+    const divC = document.getElementBy('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio)
+}
+
+    criarItemCardapio(
+        'Bolo de Chocolate',
+        'Um clássico com camadas de chocolate', 
+        'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2020/05/bolo_chocolate_leite.jpg'
+    )
